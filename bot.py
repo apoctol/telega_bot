bb2090a39b60a0c6555722e8ebc9d37d282ec386
Dimas <apoctol0795@gmail.com>
2020-05-04 21:16:30 +0300

    '+config'

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot
-
-bot = telebot.TeleBot('1227798458:AAGQv64o2Qp4nENCw5rxo1Dhp6VeyzhqWEo')
+import config
+bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
